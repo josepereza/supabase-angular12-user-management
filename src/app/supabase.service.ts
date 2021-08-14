479259e23db7b9b3f8c0733e499fa705273c4749
josepereza <josepereza66@gmail.com>
2021-08-14 09:03:51 +0200

    first2 commit

---
+++ src/app/supabase.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+import { AuthChangeEvent, createClient, Session, SupabaseClient } from '@supabase/supabase-js';
+import { environment } from '../environments/environment';
+
+export interface Profile {
+  username: string;
+  website: string;
+  avatar_url: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SupabaseService {
+  private supabase: SupabaseClient;
+  miurl:string=''
+  constructor() {
+    this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
+  }
+
+  get user() {
+    return this.supabase.auth.user();
+  }
+
+  get session() {
+    return this.supabase.auth.session();
+  }
+
+  get profile() {
+    return this.supabase
+      .from('profiles')
+      .select(`username, website, avatar_url`)
+      .eq('id', this.user?.id)
+      .single();
+  }
+
+  authChanges(callback: (event: AuthChangeEvent, session: Session | null) => void) {
+    return this.supabase.auth.onAuthStateChange(callback);
+  }
+
+  signIn(email: string) {
+    return this.supabase.auth.signIn({ email });
+  }
+
+  signOut() {
+    return this.supabase.auth.signOut();
+  }
+
+  updateProfile(profile: Profile) {
+    const update = {
+      ...profile,
+      id: this.user?.id,
+      updated_at: new Date(),
+      avatar_url:this.miurl
+    };
+
+    return this.supabase.from('profiles').upsert(update, {
+      returning: 'minimal', // Don't return the value after inserting
+    });
+  }
+
+  downLoadImage(path: string) {
+    return this.supabase.storage.from('avatars').download(path);
+  }
+
+  uploadAvatar(filePath: string, file: File) {
+    console.log(filePath,file)
+    this.miurl=filePath;
+    return this.supabase.storage.from('avatars').upload(filePath, file);
+  }
+}
